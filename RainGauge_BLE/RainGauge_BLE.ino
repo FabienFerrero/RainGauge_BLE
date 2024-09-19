a5d2e5ad1ebf7943e89ca634625e8b377785f915
Fabien Ferrero <32847282+FabienFerrero@users.noreply.github.com>
2024-09-19 10:09:39 +0200

    Add files via upload

---
+++ RainGauge_BLE/RainGauge_BLE.ino
@@ -0,0 +1,337 @@
+/**
+   '########:::::'###::::'####:'##::: ##::'######:::'##::::'##::::'###:::::'######:::'########:
+    ##.... ##:::'## ##:::. ##:: ###:: ##:'##... ##:: ##:::: ##:::'## ##:::'##... ##:: ##.....::
+    ##:::: ##::'##:. ##::: ##:: ####: ##: ##:::..::: ##:::: ##::'##:. ##:: ##:::..::: ##:::::::
+    ########::'##:::. ##:: ##:: ## ## ##: ##::'####: ##:::: ##:'##:::. ##: ##::'####: ######:::
+    ##.. ##::: #########:: ##:: ##. ####: ##::: ##:: ##:::: ##: #########: ##::: ##:: ##...::::
+    ##::. ##:: ##.... ##:: ##:: ##:. ###: ##::: ##:: ##:::: ##: ##.... ##: ##::: ##:: ##:::::::
+    ##:::. ##: ##:::: ##:'####: ##::. ##:. ######:::. #######:: ##:::: ##:. ######::: ########:
+   ..:::::..::..:::::..::....::..::::..:::......:::::.......:::..:::::..:::......::::........::
+   
+   @file RainGauge.ino
+   @author minhxl, binhphuong, fferrero
+
+   @brief This sketch help controlling on-board sensors, measuring battery level. For detail description, 
+   please visit: https://github.com/XuanMinh201/RainGauge
+
+   @version 0.0.2
+   @date 2024-06-18
+
+   @copyright Copyright (c) 2024
+
+*/
+
+extern const char *sw_version;
+
+#include "Adafruit_SHTC3.h" //http://librarymanager/All#Adafruit_SHTC3
+
+// Set pin number
+#define buttonPin P38  
+
+// Rain Gauge battery
+#define ADC_AREF 2.0f
+#define BATVOLT_R1 1.0f 
+#define BATVOLT_R2 2.0f
+#define BATVOLT_PIN A1
+
+uint16_t voltage_adc;
+uint16_t voltage;
+
+// Set Interrupt
+int ledToggle;
+int previousState = HIGH;
+unsigned int previousPress;
+volatile int buttonFlag, buttonFlag_falseDetect;
+const int buttonDebounce = 2000;
+volatile int lastDetect = 0;
+
+volatile int rainFlag;    // turn on this flag if it is rain
+volatile int notRainFlag; // turn on this flag if it is not rain
+volatile unsigned int rainGaugeCount = 0;
+unsigned long time1 = 0;
+
+uint32_t estimatedNextUplink = 0;
+int rain_count;
+// Set sensor variables
+int16_t temper;
+uint8_t humi;
+
+// Rain Stop Time
+uint64_t lastRain = 0; // the last time when it was rain
+uint64_t elapsedRain;
+uint64_t spendTime; // the remaining time before wake up in period OTAA
+
+bool bucketPositionA = false; // one of the two positions of tipping-bucket
+// const double bucketAmount = 0.01610595;   // inches equivalent of ml to trip tipping-bucket
+//#define ABP_PERIOD   (1800000) //  sleep cycle 30m
+#define ABP_PERIOD   (600000) //  sleep cycle 3m
+
+//time in case of heavy rain.
+//#define t_rain   (300000)
+#define t_rain   (120000)
+
+// #define RAIN_STOP_TIME   (6000)
+/*************************************
+
+   LoRaWAN band setting:
+     RAK_REGION_EU433
+     RAK_REGION_CN470
+     RAK_REGION_RU864
+     RAK_REGION_IN865
+     RAK_REGION_EU868
+     RAK_REGION_US915
+     RAK_REGION_AU915
+     RAK_REGION_KR920
+     RAK_REGION_AS923
+     9 (REGION_AS923_2) recommand for vietnam 
+ *************************************/
+
+ #define ABP_BAND     (RAK_REGION_EU868)
+
+#define ABP_DEVADDR  {0x26, 0x0B, 0x92, 0x34}
+#define ABP_APPSKEY  {0x88, 0x76, 0x1F, 0xFB, 0xE7, 0xB9, 0x4A, 0xAC, 0xF6, 0xD8, 0x82, 0x9E, 0xED, 0x45, 0x99, 0x83}
+#define ABP_NWKSKEY  {0x35, 0x4A, 0xD2, 0x58, 0x35, 0x23, 0x8A, 0x82, 0x14, 0xD3, 0x1B, 0x0E, 0x2E, 0x9F, 0x4F, 0x89}
+
+
+
+// #define ABP_BAND     (RAK_REGION_EU868)
+
+// #define ABP_DEVADDR  {0x00, 0x00, 0x00, 0x00}
+// #define ABP_APPSKEY  {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}
+// #define ABP_NWKSKEY  {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}
+
+
+/** Packet buffer for sending */
+uint8_t collected_data[64] = { 0 };
+
+Adafruit_SHTC3 shtc3 = Adafruit_SHTC3();
+
+void recvCallback(SERVICE_LORA_RECEIVE_T * data)
+{
+  if (data->BufferSize > 0) {
+    Serial.println("Something received!");
+    for (int i = 0; i < data->BufferSize; i++) {
+      Serial.printf("%x", data->Buffer[i]);
+    }
+    Serial.print("\r\n");
+  }
+}
+
+void sendCallback(int32_t status)
+{
+  if (status == 0) {
+    Serial.println("Successfully sent");
+  } else {
+    Serial.println("Sending failed");
+  }
+}
+
+void setup()
+{
+  Serial.begin(115200, RAK_AT_MODE);
+
+  // Initialize Interrupt
+  Serial.println("RAKwireless Arduino Interrupt Example");
+  Serial.println("------------------------------------------------------");
+  pinMode(buttonPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(buttonPin), button_ISR, FALLING);
+
+  buttonFlag = 0;
+  buttonFlag_falseDetect = 0;
+  lastDetect = 0;
+
+  analogReadResolution(10);
+  
+  // Initialize SHTC3
+  Serial.println("SHTC3 test");
+  if (!shtc3.begin())
+  {
+    Serial.println("Couldn't find SHTC3");
+     //while (1) delay(1);
+  }
+  Serial.println("Found SHTC3 sensor");
+
+   // Wake-up
+  api.system.sleep.setup(RUI_WAKEUP_FALLING_EDGE, A1);
+
+    // ABP Device Address MSB first
+  uint8_t node_dev_addr[4] = ABP_DEVADDR;
+  // ABP Application Session Key
+  uint8_t node_app_skey[16] = ABP_APPSKEY;
+  // ABP Network Session Key
+  uint8_t node_nwk_skey[16] = ABP_NWKSKEY;
+
+    // OTAA Application EUI MSB
+ uint8_t node_device_eui[8] = {0x70, 0xB3, 0xD5, 0x7E, 0xD0, 0x06, 0x9C, 0xE0};
+ // OTAA Application Key MSB  
+ uint8_t node_app_eui[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+ uint8_t node_app_key[16] = {0xCD, 0x88, 0x25, 0x6E, 0x47, 0x7F, 0xF7, 0x73, 0xBD, 0x32, 0x00, 0x73, 0x2A, 0x17, 0x49, 0x94};
+
+
+  if (!api.lorawan.daddr.set(node_dev_addr, 4)) {
+    Serial.printf("LoRaWan ABP - set device addr is incorrect! \r\n");
+    return;
+  }
+  if (!api.lorawan.appskey.set(node_app_skey, 16)) {
+    Serial.printf
+  ("LoRaWan ABP - set application session key is incorrect! \r\n");
+    return;
+  }
+  if (!api.lorawan.nwkskey.set(node_nwk_skey, 16)) {
+    Serial.printf
+  ("LoRaWan ABP - set network session key is incorrect! \r\n");
+    return;
+  }
+  if (!api.lorawan.band.set(ABP_BAND)) {
+    Serial.printf("LoRaWan ABP - set band is incorrect! \r\n");
+    return;
+  }
+  if (!api.lorawan.deviceClass.set(RAK_LORA_CLASS_A)) {
+    Serial.printf("LoRaWan ABP - set device class is incorrect! \r\n");
+    return;
+  }
+  if (!api.lorawan.njm.set(RAK_LORA_ABP)) // Set the network join mode to ABP
+  {
+    Serial.
+  printf("LoRaWan ABP - set network join mode is incorrect! \r\n");
+    return;
+  }
+  if (!api.lorawan.dr.set(3)) // Set the network join mode to ABP
+  {
+    Serial.
+  printf("Setting DR is incorrect! \r\n");
+    return;
+  }
+  
+  /** Check LoRaWan Status*/
+  Serial.printf("Duty cycle is %s\r\n", api.lorawan.dcs.get()? "ON" : "OFF"); // Check Duty Cycle status
+  
+  uint8_t assigned_dev_addr[4] = { 0 };
+  api.lorawan.daddr.get(assigned_dev_addr, 4);
+  Serial.printf("Device Address is %02X%02X%02X%02X\r\n", assigned_dev_addr[0], assigned_dev_addr[1], assigned_dev_addr[2], assigned_dev_addr[3]);  // Check Device Address
+  Serial.printf("Uplink period is %ums\r\n", ABP_PERIOD);
+  Serial.println("");
+  api.lorawan.registerRecvCallback(recvCallback);
+  api.lorawan.registerSendCallback(sendCallback);
+
+char ble_name[12] = { 'L', 'O', 'R', 'A', '-', 'A', 'P', 'O', 'L', 'L', 'O', '\0' };
+uint8_t device_name_length = 12;
+  api.ble.settings.broadcastName.set(ble_name, device_name_length); 
+   uint32_t baudrate = Serial.getBaudrate();
+    Serial.begin(baudrate);
+    Serial6.begin(baudrate, RAK_AT_MODE);
+      
+    Serial.println("RAKwireless RAK11720");
+    Serial.println("------------------------------------------------------");
+    Serial.printf("Version: %s\r\n", sw_version);
+  
+    // Start BLE UART advertisement for 30 seconds
+    api.ble.settings.blemode(RAK_BLE_UART_MODE);
+    api.ble.advertise.start(30);  
+
+}
+
+void uplink_routine()
+{
+  /** Payload of Uplink */
+  uint8_t data_len = 0;
+  collected_data[data_len++] = (uint8_t)buttonFlag;
+  collected_data[data_len++] = temper >> 8;
+  collected_data[data_len++] = temper & 0xFF;
+  collected_data[data_len++] = (uint8_t)humi & 0xFF;
+  collected_data[data_len++] = voltage >> 8;
+  collected_data[data_len++] = voltage & 0xFF;
+
+  Serial.println("Data Packet:");
+  for (int i = 0; i < data_len; i++)
+  {
+    Serial.printf("0x%02X ", collected_data[i]);
+  }
+  Serial.println("");
+
+  /** Send the data package */
+  if (api.lorawan.send(data_len, (uint8_t *)&collected_data, 2, false, 1))
+  {
+    Serial.println("Sending is requested");
+  }
+  else
+  {
+    Serial.println("Sending failed");
+  }
+}
+
+void loop()
+{
+  // Read SHTC3
+ 
+  
+  //
+  sensors_event_t humidity, temp;
+  shtc3.getEvent(&humidity, &temp); // populate temp and humidity objects with fresh data
+  temper = (int)(temp.temperature * 100) ;
+  humi = (humidity.relative_humidity*2);
+  Serial.print("Sensors values : temp = ");
+  Serial.print(temper/100);
+  Serial.println("deg");
+  Serial.print("hum= ");
+  Serial.print(humi/2);
+  Serial.println("%");
+
+  
+  Serial.print("Value: ");
+  Serial.println(buttonFlag);
+  Serial.print(" | False Detect: ");
+  Serial.println(buttonFlag_falseDetect);
+
+  //Battery variables
+  voltage_adc = (uint16_t)analogRead(BATVOLT_PIN);
+  voltage = (int16_t)(voltage_adc* ADC_AREF / 1.024) * (BATVOLT_R1 + BATVOLT_R2) / (BATVOLT_R2);
+
+  Serial.print("Voltage: ");
+  Serial.println(voltage);
+
+  Serial.print("Voltage ADC: ");
+  Serial.println(voltage_adc);
+
+
+  // LoRaWAN Uplink
+  uplink_routine();
+  buttonFlag = 0;
+  rain_count = 0;
+
+  // Set sleep until the next LoRaWAN Uplink
+  Serial.printf("Try sleep %ums..", ABP_PERIOD);
+  estimatedNextUplink = millis() + ABP_PERIOD;
+  api.system.sleep.all(ABP_PERIOD);
+
+  // Re-check the wake up reason. If the wakeup caused by External Interrupt, go back to sleep mode
+  while (estimatedNextUplink > millis())
+  {
+    uint32_t remainingSleepTime = estimatedNextUplink - millis();
+    api.system.sleep.all(remainingSleepTime);
+  }
+
+  Serial.println("Wakeup..");
+}
+
+void button_ISR()
+{
+  if (rain_count == 0){
+    estimatedNextUplink = estimatedNextUplink - (ABP_PERIOD - t_rain);
+  }
+  rain_count++;
+  int _now = millis();
+  if ((_now - lastDetect) < buttonDebounce)
+  {
+    lastDetect = _now;
+    buttonFlag++;
+    rain_count = rain_count -1;
+  }
+  if (buttonFlag > 3) {
+    buttonFlag = 0;
+    rain_count = rain_count -1; // remove the first count
+    // Start BLE UART advertisement for 30 seconds
+    api.ble.advertise.start(30);     
+  } 
+
+}
